93abed7f4887469c508cfaf50382d0abf7e38f42
Tyler Keesling <tyler.keesling@okta.com>
2023-01-21 17:00:05 -0500

    Server listening on port log

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-const tikTokEndpoint = 'https://open-api.tiktok.com/oauth/access_token';
+const TIKTOK_ACCESS_TOKEN_ENDPOINT = 'https://open-api.tiktok.com/oauth/access_token';
+const PORT = 3333;
 
-app.post('/tiktok/token', async (req, res) => {
+app.post('/proxy/token', async (req, res) => {
   const params = new URLSearchParams({
     client_key: req.body.client_id,
     client_secret: req.body.client_secret,
@@ -16,11 +17,11 @@ app.post('/tiktok/token', async (req, res) => {
     grant_type: 'authorization_code',
   }).toString();
 
-  const url = `${tikTokEndpoint}?${params}`;
+  const url = `${TIKTOK_ACCESS_TOKEN_ENDPOINT}?${params}`;
   const response = await fetch(url, { method: 'POST' });
   const data = await response.json();
 
   res.json(data.data);
 });
 
-app.listen(3333);
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
